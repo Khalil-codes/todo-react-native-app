4948b563f70bcaa4b3f6578f4a7905f1f15177ea
khalil-codes <khalilpati55@gmail.com>
2022-03-21 22:35:18 +0530

    Initialize project using react-native-cli

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { SafeAreaView } from "react-native";
+import Form from "./src/screens/Login";
+
+const App = () => {
+    return (
+        <SafeAreaView>
+            <Form />
+        </SafeAreaView>
+    );
+};
+
+export default App;
