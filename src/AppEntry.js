744f1d11e53b0c1564de59017d0abef3d8267b28
khalil-codes <khalilpati55@gmail.com>
2022-03-22 18:14:36 +0530

    Added Protected Screens and Stack navigation

---
+++ src/AppEntry.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import Login from "./screens/Login";
+import Register from "./screens/Register";
+import { useAuth } from "./redux/store";
+import HomeScreen from "./screens/HomeScreen";
+import { useDispatch } from "react-redux";
+import { logoutUser } from "./redux/auth/authSlice";
+import Logout from "./components/Logout";
+import TabsScreen from "./screens/TabsScreen";
+
+const Stack = createNativeStackNavigator();
+
+const AppEntry = () => {
+    const { user } = useAuth();
+    const dispatch = useDispatch();
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                {user ? (
+                    <Stack.Screen
+                        name="Dashboard"
+                        component={TabsScreen}
+                        options={{
+                            headerRight: () => <Logout />,
+                        }}
+                    />
+                ) : (
+                    <>
+                        <Stack.Screen name="Login" component={Login} />
+                        <Stack.Screen name="Register" component={Register} />
+                    </>
+                )}
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
+
+export default AppEntry;
