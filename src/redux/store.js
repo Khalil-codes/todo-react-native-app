6d30179491ccab987e6d71fb6dec8d0030ebb580
khalil-codes <khalilpati55@gmail.com>
2022-03-23 16:07:57 +0530

    Added reducers in store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { useSelector } from "react-redux";
 import authReducer from "./auth/authSlice";
+import todoReducer from "./todo/todoSlice";
+
 export const store = configureStore({
     reducer: {
         auth: authReducer,
+        todos: todoReducer,
     },
 });
 
 export const useAuth = () => useSelector((state) => state.auth);
+export const useTodos = () => useSelector((state) => state.todos);
