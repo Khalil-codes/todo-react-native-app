d923ade9f275e5b69bc94465175554fc64c67f1f
khalil-codes <khalilpati55@gmail.com>
2022-03-23 12:01:38 +0530

    Added Global Color Object

---
+++ src/Colors.js
@@ -0,0 +1,10 @@
+export default Colors = {
+    primaryColor: "#4D96FF",
+    secondaryColor: "#FFD93D",
+    disabledColor: "#88b9ff",
+    successColor: "#6BCB77",
+    errorColor: "#FF6B6B",
+    darkColor: "#000000",
+    lightColor: "#ffffff",
+    borderColor: "#cccccc",
+};
