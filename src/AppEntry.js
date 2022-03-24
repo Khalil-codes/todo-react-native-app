7591db219b4bc28bb292e244c0e5858a69176123
khalil-codes <khalilpati55@gmail.com>
2022-03-24 11:22:29 +0530

    Added useEffect to get todos from server

--- src/AppEntry.js
+++ src/AppEntry.js
@@ -1,22 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import Login from "./screens/Login";
 import Register from "./screens/Register";
 import { useAuth } from "./redux/store";
-import HomeScreen from "./screens/HomeScreen";
-import { useDispatch } from "react-redux";
-import { logoutUser } from "./redux/auth/authSlice";
 import Logout from "./components/Logout";
 import TabsScreen from "./screens/TabsScreen";
 import Colors from "./Colors";
+import { useDispatch } from "react-redux";
+import { getTodos } from "./redux/todo/todoSlice";
 
 const Stack = createNativeStackNavigator();
 
 const AppEntry = () => {
-    const { user } = useAuth();
     const dispatch = useDispatch();
+    const { user } = useAuth();
+    useEffect(() => {
+        const unsub = async () => {
+            dispatch(getTodos());
+        };
+        unsub();
+    }, []);
     return (
         <NavigationContainer>
             <Stack.Navigator
